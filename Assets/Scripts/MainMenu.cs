a54f17bac6e0106b82f7a45a8cd9132d0e7a9272
Jethro Moller <jethrojmoller@gmail.com>
2018-04-07 02:41:26 +0200

    Added menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void PlaySingle(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	}
+
+	public void Quit(){
+		Application.Quit();
+		Debug.Log("Game quitted");
+
+	}
+}
